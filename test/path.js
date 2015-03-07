1a3ababa0eaccf259155840ecd38a529e3db050c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-07 16:17:55 -0500

    adds `ext` and `segments` helpers, tests

--- test/path.js
+++ test/path.js
@@ -7,44 +7,78 @@
 
 'use strict';
 
-require('should');
-var helpers = require('..');
 var _ = require('lodash');
+var path = require('path');
+var helpers = require('..');
+require('should');
 
 var imports = {imports: helpers};
 
 describe('path helpers', function() {
-  it('should join the given paths:', function() {
-    _.template('<%= path.join("a", "b") %>', imports)().should.equal('a/b');
+  describe('join', function() {
+    it('should join the given paths:', function() {
+      _.template('<%= path.join("a", "b") %>', imports)().should.equal('a/b');
+    });
   });
-  it('should return the dirname:', function() {
-    _.template('<%= path.dirname("a/b/c/e.js") %>', imports)().should.equal('a/b/c');
+  describe('dirname', function() {
+    it('should return the dirname:', function() {
+      _.template('<%= path.dirname("a/b/c/e.js") %>', imports)().should.equal('a/b/c');
+    });
   });
-  it('should return the basename:', function() {
-    _.template('<%= path.basename("a/b/c/e.js") %>', imports)().should.equal('e.js');
+  describe('basename', function() {
+    it('should return the basename:', function() {
+      _.template('<%= path.basename("a/b/c/e.js") %>', imports)().should.equal('e.js');
+    });
   });
-  it('should return the filename:', function() {
-    _.template('<%= path.filename("a/b/c/e.js") %>', imports)().should.equal('e');
+  describe('filename', function() {
+    it('should return the filename:', function() {
+      _.template('<%= path.filename("a/b/c/e.js") %>', imports)().should.equal('e');
+    });
   });
-  it('should return the extname:', function() {
-    _.template('<%= path.extname("a/b/c/e.js") %>', imports)().should.equal('.js');
+  describe('extname', function() {
+    it('should return the extname with dot:', function() {
+      _.template('<%= path.extname("a/b/c/e.js") %>', imports)().should.equal('.js');
+    });
   });
-  it('should return the extname of the given path:', function() {
-    _.template('<%= path.isAbsolute("/foo/bar") %>', imports)().should.equal('true');
-    _.template('<%= path.isAbsolute("/baz/..") %>', imports)().should.equal('true');
-    _.template('<%= path.isAbsolute("qux/") %>', imports)().should.equal('false');
-    _.template('<%= path.isAbsolute(".") %>', imports)().should.equal('false');
-    _.template('<%= path.isAbsolute("//server") %>', imports)().should.equal('true');
-    _.template('<%= path.isAbsolute("bar\\baz") %>', imports)().should.equal('false');
-    _.template('<%= path.isAbsolute(".") %>', imports)().should.equal('false');
+  describe('ext', function() {
+    it('should return the extension without dot:', function() {
+      _.template('<%= path.ext("a/b/c/e.js") %>', imports)().should.equal('js');
+    });
   });
+  describe('segments', function() {
+    it('should return specified path segments:', function() {
+      _.template('<%= path.segments("a/b/c/e.js", 1, 3) %>', imports)().should.equal('b/c');
+      _.template('<%= path.segments("a/b/c/e.js", 1, 2) %>', imports)().should.equal('b');
+      _.template('<%= path.segments("a/b/c/e.js", 0, 3) %>', imports)().should.equal('a/b/c');
+      _.template('<%= path.segments("a/b/c/e.js", 2, 3) %>', imports)().should.equal('c');
+      _.template('<%= path.segments("a/b/c/e.js", 0, 3) %>', imports)().should.equal('a/b/c');
+    });
 
-  it('should resolve the given paths:', function() {
-    _.template('<%= path.extname("c/e.js") %>', imports)().should.equal('.js');
+    it('should disregard extra slashes:', function() {
+      _.template('<%= path.segments("a///b\\/c\\/e.js", 1, 3) %>', imports)().should.equal('b/c');
+      _.template('<%= path.segments("a///b/\\//c/e.js", 1, 2) %>', imports)().should.equal('b');
+    });
   });
-
-  it('should return a relative file path:', function() {
-    _.template('<%= path.relative("docs/", "docs/a/b.js") %>', imports)().should.equal('a/b.js');
-    _.template('<%= path.relative("docs/a/b.js", "docs/") %>', imports)().should.equal('..');
+  describe('isAbsolute', function() {
+    it('should return the extname of the given path:', function() {
+      _.template('<%= path.isAbsolute("/foo/bar") %>', imports)().should.equal('true');
+      _.template('<%= path.isAbsolute("/baz/..") %>', imports)().should.equal('true');
+      _.template('<%= path.isAbsolute("qux/") %>', imports)().should.equal('false');
+      _.template('<%= path.isAbsolute(".") %>', imports)().should.equal('false');
+      _.template('<%= path.isAbsolute("//server") %>', imports)().should.equal('true');
+      _.template('<%= path.isAbsolute("bar\\baz") %>', imports)().should.equal('false');
+      _.template('<%= path.isAbsolute(".") %>', imports)().should.equal('false');
+    });
+  });
+  describe('extname', function() {
+    it('should resolve the given path(s):', function() {
+      _.template('<%= path.resolve("c/e.js") %>', imports)().should.equal(path.resolve('c/e.js'));
+    });
+  });
+  describe('relative', function() {
+    it('should return a relative file path:', function() {
+      _.template('<%= path.relative("docs/", "docs/a/b.js") %>', imports)().should.equal('a/b.js');
+      _.template('<%= path.relative("docs/a/b.js", "docs/") %>', imports)().should.equal('..');
+    });
   });
 });
