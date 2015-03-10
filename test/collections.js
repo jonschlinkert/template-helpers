e93f6749756b18d17143e5e2cd1fa0912cc2cba6
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-09 23:16:20 -0400

    tests for `any`

---
+++ test/collections.js
@@ -0,0 +1,30 @@
+/*!
+ * template-helpers <https://github.com/jonschlinkert/template-helpers>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+require('should');
+var helpers = require('..');
+var _ = require('lodash');
+
+var imports = {imports: helpers.collections};
+
+describe('collections', function() {
+  describe('any', function() {
+    it('should return if a value exists in the given string.', function() {
+      _.template('<%= any("a-b-c", "a") %>', imports)(context).should.equal('true');
+      _.template('<%= any("a-b-c", "d") %>', imports)(context).should.equal('false');
+    });
+    it('should return if a value exists in the given object.', function() {
+      _.template('<%= any({a: "b", c: "d"}, "a") %>', imports)(context).should.equal('true');
+      _.template('<%= any([{a: "b", c: "d"}], {a: "b"}) %>', imports)(context).should.equal('true');
+    });
+    it('should return if a value exists in the given array.', function() {
+      _.template('<%= any("a-b-c", "d") %>', imports)(context).should.equal('false');
+    });
+  });
+});
