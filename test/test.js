ee24a6ea5d6405ca141447277a588d3a2817e111
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-26 17:18:53 -0500

    adds unit tests for main export/helper groups

--- test/test.js
+++ test/test.js
@@ -27,9 +27,19 @@ describe('helpers', function() {
     assert(typeof helpers.array === 'object');
   });
 
+  it('should filter out array helpers when passed as an argument to lib', function() {
+    var array = lib('array');
+    assert(typeof array === 'object');
+  });
+
   it('should expose code helpers on `code`', function() {
     assert(helpers.code);
     assert(typeof helpers.code === 'object');
+  });
+
+  it('should filter out code helpers when passed as an argument to lib', function() {
+    var code = lib('code');
+    assert(typeof code === 'object');
   });
 
   it('should expose collection helpers on `collection`', function() {
@@ -37,9 +47,19 @@ describe('helpers', function() {
     assert(typeof helpers.collection === 'object');
   });
 
+  it('should filter out collection helpers when passed as an argument to lib', function() {
+    var collection = lib('collection');
+    assert(typeof collection === 'object');
+  });
+
   it('should expose conditional helpers on `conditional`', function() {
     assert(helpers.conditional);
     assert(typeof helpers.conditional === 'object');
+  });
+
+  it('should filter out conditional helpers when passed as an argument to lib', function() {
+    var conditional = lib('conditional');
+    assert(typeof conditional === 'object');
   });
 
   it('should expose fs helpers on `fs`', function() {
@@ -47,9 +67,19 @@ describe('helpers', function() {
     assert(typeof helpers.fs === 'object');
   });
 
+  it('should filter out fs helpers when passed as an argument to lib', function() {
+    var fs = lib('fs');
+    assert(typeof fs === 'object');
+  });
+
   it('should expose html helpers on `html`', function() {
     assert(helpers.html);
     assert(typeof helpers.html === 'object');
+  });
+
+  it('should filter out html helpers when passed as an argument to lib', function() {
+    var html = lib('html');
+    assert(typeof html === 'object');
   });
 
   it('should expose math helpers on `math`', function() {
@@ -57,13 +87,47 @@ describe('helpers', function() {
     assert(typeof helpers.math === 'object');
   });
 
+  it('should filter out math helpers when passed as an argument to lib', function() {
+    var math = lib('math');
+    assert(typeof math === 'object');
+  });
+
   it('should expose object helpers on `object`', function() {
     assert(helpers.object);
     assert(typeof helpers.object === 'object');
   });
 
+  it('should filter out object helpers when passed as an argument to lib', function() {
+    var object = lib('object');
+    assert(typeof object === 'object');
+  });
+
+  it('should expose path helpers on `path`', function() {
+    assert(helpers.path);
+    assert(typeof helpers.path === 'object');
+  });
+
+  it('should filter out path helpers when passed as an argument to lib', function() {
+    var path = lib('path');
+    assert(typeof path === 'object');
+  });
+
   it('should expose string helpers on `string`', function() {
     assert(helpers.string);
     assert(typeof helpers.string === 'object');
+  });
+
+  it('should filter out string helpers when passed as an argument to lib', function() {
+    var string = lib('string');
+    assert(typeof string === 'object');
+    assert(typeof string.lower === 'function');
+    assert(typeof string.upper === 'function');
+  });
+
+  it('should filter out an array of groups', function() {
+    var helpers = lib(['string', 'array']);
+    assert(typeof helpers === 'object');
+    assert(typeof helpers.last === 'function');
+    assert(typeof helpers.upper === 'function');
   });
 });
